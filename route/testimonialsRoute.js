fbd49dc876948dfc1a94ee0008e4a8b1aa0c94fa
SindileM <smakhathini24@gmail.com>
2022-02-11 16:32:27 +0200

    make it better

--- route/testimonialsRoute.js
+++ route/testimonialsRoute.js
@@ -4,37 +4,50 @@ const fixArrayId = require('../helpers');
 let testimonials= [
     { 
         "id": "1",
-        "img_link": "",
-        "name": "",
-        "text": "",
+        "img_link": "https://i.postimg.cc/k5GFYrrd/Alex-Sexwale.jpg",
+        "title": "Lecturer",
+        "name": "Alex Sexwale",
+        "text": "Sindile is a bright and talented individual. She brings joy to all her peers and works hard.",
         "email": ""
     },
     { 
         "id": "2",
-        "img_link": "",
-        "name": "",
-        "text": "",
+        "img_link": "https://i.postimg.cc/DwVNCKPb/craigee-min.jpg",
+        "title": "Colleague",
+        "name": "Craig Braaf",
+        "text": "Sindile is a bright soul and embodies it too. She is quick, witty and funny and always brings her A game when participating in group work and personal projects. She possesses immense confidence within herself and it reflects in her work. She never steers away from helping others and putting a smile on your face. Any company would be lucky to call Sindile their employee.",
         "email": ""
     },
     { 
         "id": "3",
-        "img_link": "",
-        "name": "",
-        "text": "",
+        "img_link": "https://i.postimg.cc/dQZPnncp/Emihle2-min.jpg",
+        "title": "Colleague",
+        "name": "Emihle Cebisa",
+        "text": "Sindile is a people's person she is reliable,trustworthy and professional  she would be a great asset to any company she joins and personally i wish her nothing but the best in he career.",
         "email": ""
     },
     { 
         "id": "4",
-        "img_link": "",
-        "name": "",
-        "text": "",
+        "img_link": "https://i.postimg.cc/sx4JKtj5/tursha-min.png",
+        "title": "Colleague",
+        "name": "Tursha Arendse",
+        "text": "This young lady is what one will describe as a ray of sunshine. She honestly has the best personality which makes her such a joy to work with. But don't let her loving personality fool you because she is a tough cookie too. When i think of hard-working and determined I think of Sindi. You won't regret working with her.",
         "email": ""
     },
     { 
         "id": "5",
-        "img_link": "",
-        "name": "",
-        "text": "",
+        "img_link": "https://i.postimg.cc/JhT03KCt/Lindokuhle3-min.jpg",
+        "title": "Colleague",
+        "name": "Lindokuhle Nkamela",
+        "text": "I met Sindile from life choices academy, she is one of the smart, hard working ladies i ever come across. She is a people's person. She really enjoying doing public speaking. Cindy is very dedicated, kind and has a good ear when you need her.",
+        "email": ""
+    },
+    { 
+        "id": "6",
+        "img_link": "https://i.postimg.cc/y6X1Fs4W/Zanele1-min.jpg",
+        "title": "Colleague",
+        "name": "Ntombizanele Moni",
+        "text": "Sindile is someone who I am always happy to be around. She knows when to have fun and when to work hard.",
         "email": ""
     },
 ];
